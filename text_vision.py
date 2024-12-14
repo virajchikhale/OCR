824d6ebf3b9dac34d78a829130ce37ef5fbdf7e6
Viraj Chikhale <127619412+virajchikhale@users.noreply.github.com>
2024-12-14 02:19:45 +0000

    git secrets

--- text_vision.py
+++ text_vision.py
@@ -18,10 +18,16 @@ import io
 
 from dotenv import load_dotenv, dotenv_values 
 load_dotenv() 
-api_key = st.secrets["GEMINI_API_KEY"]
+# api_key = st.secrets["GEMINI_API_KEY"]
+import os
+
+# Access secrets as environment variables
+api_key = os.getenv("GEMINI_API_KEY")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("VISION_API")
+
 
 # Set up Google Cloud credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets["private_key"]
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets["private_key"]
 
 # def extract_text_from_docx(docx_path):
 #     """Extract text from a DOCX file."""
