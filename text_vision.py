c8d6e9d19a7e8d2388777de902189e87d6c5614a
Viraj Chikhale <127619412+virajchikhale@users.noreply.github.com>
2024-12-29 23:10:55 +0530

    up

--- text_vision.py
+++ text_vision.py
@@ -27,7 +27,20 @@ load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
 
 # Set up Google Cloud credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets['google']['GOOGLE_APPLICATION_CREDENTIALS']
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 
+{
+  "type": st.secrets["type"]
+  "project_id": st.secrets["project_id"]
+  "private_key_id": st.secrets["private_key_id"]
+  "private_key": st.secrets["private_key"]
+  "client_email": st.secrets["client_email"]
+  "client_id": st.secrets["client_id"]
+  "auth_uri": st.secrets["auth_uri"]
+  "token_uri": st.secrets["token_uri"]
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"]
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+  "universe_domain": st.secrets["universe_domain"]
+}
 
 
 def extract_text_from_pdf(content):
